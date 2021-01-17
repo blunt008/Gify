31c017eb578fb69290faafe150839f329f8e78ad
Damian Tabor <zettie008@gmail.com>
2021-01-17 23:56:45 +0000

    Added test to see if user can go from login to register page

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,8 +11,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     
     def test_login_page_is_displayed(self):
+        """
+        Open website and check if login page is rendered
+        and 'Login' is shown as page title
+        """
         self.browser.get("http://localhost:8000")
         self.assertIn("Login", self.browser.title)
+
+    def test_user_can_open_register_page(self):
+        """
+        Check if user can go from login page to register
+        """
+        self.browser.get("http://localhost:8000")
+        register_button = self.browser.find_element_by_tag_name("a")
+        register_button.click()
+        time.sleep(1)
+        self.assertIn("Create an account", self.browser.title)
+
+
     
     def tearDown(self):
         self.browser.quit()
