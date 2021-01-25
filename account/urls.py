04d845e384460a31fb9c6bc294de7dd95c31c7e8
Damian Tabor <zettie008@gmail.com>
2021-01-25 22:37:24 +0000

    Added new views to urls

--- account/urls.py
+++ account/urls.py
@@ -22,10 +22,12 @@ urlpatterns = [
                  auth_views.PasswordResetDoneView.as_view(),
                  name="password_reset_done"),
             path("reset/<uidb64>/<token>/",
-                 auth_views.PasswordResetConfirmView.as_view(),
+                 views.MyPasswordResetConfirmView.as_view(),
                  name="password_reset_confirm"),
             path("reset/done/",
-                 auth_views.PasswordResetCompleteView.as_view(),
+                 auth_views.PasswordResetCompleteView.as_view(
+                     template_name="registration/password_reset_complete.html"
+                 ),
                  name="password_reset_complete"),
             path("profile/<str:name>/",
                  views.profile,
