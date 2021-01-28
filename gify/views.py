b778e47639fe4e53d9890961256ae6feeac904a5
Damian Tabor <zettie008@gmail.com>
2021-01-28 21:58:30 +0000

    Created initial laout for gif feed

--- gify/views.py
+++ gify/views.py
@@ -6,7 +6,19 @@ def index(request):
     """
     Index page
     """
+    posts = [
+        {
+            "author": "Damian",
+            "link": "www.someurl.com",
+            "date": "2012-12-12"
+        },
+        {
+            "author": "zettie",
+            "link": "www.another-url.com",
+            "date": "2020-9-12"
+        }
+    ]
     return render(request,
                   "gify/home.html",
-                  {"section": "index"})
+                  {"section": "index", "posts": posts})
 
