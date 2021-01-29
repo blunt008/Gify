e145a9d090c9f0c862df212643c91a6a903e0f87
Damian Tabor <zettie008@gmail.com>
2021-01-29 23:04:04 +0000

    Added pagination and create async function for fetching extra posts via AJAX

--- gify/views.py
+++ gify/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
@@ -8,8 +10,24 @@ def index(request):
     """
     Index page
     """
-    videos = Post.objects.all()
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 3)
+    page = request.GET.get("page", "")
+    try:
+        posts = paginator.page(page)
+        if request.META["CONTENT_TYPE"] == "application/json":
+            return render(
+                request,
+                "gify/list_ajax.html",
+                {"posts": posts}
+            )
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        return HttpResponse("")
+
     return render(request,
                   "gify/home.html",
-                  {"section": "index", "videos": videos})
+                  {"section": "index", "posts": posts})
 
