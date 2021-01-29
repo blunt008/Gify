eda8f703c0853f4bfe68a1b75d8e4870c6c4fc05
Damian Tabor <zettie008@gmail.com>
2021-01-29 22:15:35 +0000

    Added styles to container and working on infinite scroll

--- gify/views.py
+++ gify/views.py
@@ -8,7 +8,7 @@ def index(request):
     """
     Index page
     """
-    videos = Video.objects.all()
+    videos = Video.objects.all()[:3]
     return render(request,
                   "gify/home.html",
                   {"section": "index", "videos": videos})
