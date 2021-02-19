82c38544892c15beae526efb37cc1edee983d39e
Damian Tabor <zettie008@gmail.com>
2021-02-19 19:47:58 +0000

    Add helper functions for getting embedded URLs

--- gify/forms.py
+++ gify/forms.py
@@ -12,16 +12,39 @@ class PostCreateForm(forms.ModelForm):
 
     def clean_link(self):
         """
-        Check if the link provided points
-        to streamable or youtube and raise validation errors
-        if it doesn't
+        Check if URL links to supported service and transform it
+        to embedded version
         """
         link = self.cleaned_data.get("link", "");
-        print(link)
-        if "you" not in link and "stream" not in link:
-            print("error")
+
+        if "you" in link:
+            link = self.youtube_embed(link)
+        elif "stream" in link:
+            link = self.streamable_embed(link)
+        else:
             raise forms.ValidationError(("We only accept URLs to YouTube"
                                          " or Streamable"))
 
         return link
 
+    def youtube_embed(self, url: str) -> str:
+        """
+        Transform standard youtube URL
+        into embedded version
+        """
+        YOUTUBE_EMBED_URL = "https://www.youtube.com/embed/"
+        link_id = url.split("/")[-1]
+        if "?" in link_id:
+            link_id = link_id.split("?")[0]
+
+        return YOUTUBE_EMBED_URL + link_id
+
+    def streamable_embed(self, url: str) -> str:
+        """
+        Transform standard streamable URL
+        into embedded version
+        """
+        STREAMABLE_EMBED_URL = "https://streamable.com/e/"
+        link_id = url.split("/")[-1]
+
+        return STREAMABLE_EMBED_URL + link_id
