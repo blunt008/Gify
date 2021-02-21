a6730875d53957807dc4c56602a4f924e1b37094
Damian Tabor <zettie008@gmail.com>
2021-02-21 20:26:17 +0000

    Remove old code

--- gify/static/js/show_comments.js
+++ gify/static/js/show_comments.js
@@ -1,6 +1,3 @@
-const comments = document.querySelectorAll(".comment_button");
-
-
 document.body.addEventListener('click', event => {
 	if (event.srcElement.className === 'comment_button') {
 		console.log('shows comments');
