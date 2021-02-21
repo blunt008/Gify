c60dcde7cb58407715d0ffcea425a0d3186bfa4f
Damian Tabor <zettie008@gmail.com>
2021-02-21 20:54:15 +0000

    Add function for searching and retrieving post div

--- gify/static/js/show_comments.js
+++ gify/static/js/show_comments.js
@@ -1,5 +1,27 @@
+const template = document.getElementById('add-comment');
+
+
 document.body.addEventListener('click', event => {
 	if (event.srcElement.className === 'comment_button') {
-		console.log('shows comments');
+		const clone = document.importNode(template.content, true);
+		const postDiv = getPostDiv(event.target);
+		console.log(postDiv);
 	}
 })
+
+
+/*
+ * Receives clicked on button
+ * and returns its parent 'post' div
+ */
+const getPostDiv = (element) => {
+	// Search for parent post div up to 'height' elements in DOM
+	let height = 4;
+	while (height > 0) {
+		element = element.parentNode;
+		if (element.className.includes('card post')) {
+			return element;
+		}
+		height--;	
+	}
+}
