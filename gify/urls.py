11026e19c297aeeef0c06c2959ad7830fb799eb9
Damian Tabor <zettie008@gmail.com>
2021-03-14 19:41:38 +0000

    Add URL pattern for adding comments

--- gify/urls.py
+++ gify/urls.py
@@ -7,5 +7,6 @@ app_name = "gify"
 urlpatterns = [
     path("create/", views.post_create, name="create"),
     path("get_comments/", views.retrieve_comments, name="get_comments"),
+    path("comment/add/", views.add_comment, name="add_comment"),
     path("", views.index, name="index"),
 ]
