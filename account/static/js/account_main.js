47f6b6baf569f42d839b50d69ac101524377aa12
Damian Tabor <zettie008@gmail.com>
2021-03-18 19:35:54 +0000

    Remove redundant function

--- account/static/js/account_main.js
+++ account/static/js/account_main.js
@@ -73,26 +73,6 @@ const removeAvatar = (event, div, avatarCount) => {
 
 
 /*
- * Retrieve csrfToken
- */
-const getCookie = (name) => {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-};
-
-
-/*
  * Retrieve all avatars available for user
  */
 const requestAvatars = async (event) => {
