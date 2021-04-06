04dc9277fe449c87db6dc4495f117d80dfd81c30
Damian Tabor <zettie008@gmail.com>
2021-04-06 19:38:04 +0100

    Add action model to admin panel

--- gify/admin.py
+++ gify/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Action
 
 # Register your models here.
 
@@ -8,3 +8,10 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ["created"]
     list_filter = ["created"]
+
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'verb', 'target', 'created')
+    list_filter = ('created',)
+    search_fields = ('verb',)
