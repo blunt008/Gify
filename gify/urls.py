8bc7f1f72467e92f6654cdb3b9b1c2dbedb1db0f
Damian Tabor <zettie008@gmail.com>
2021-04-06 18:38:50 +0100

    Add url pattern for wall view

--- gify/urls.py
+++ gify/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("get_comments/", views.retrieve_comments, name="get_comments"),
     path("comment/add/", views.add_comment, name="add_comment"),
     path('like/', views.like_unlike, name='like_unlike'),
+    path('wall/', views.wall, name='wall'),
     path("", views.index, name="index"),
 ]
